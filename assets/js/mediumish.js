cb34bf207ed11230b6c182dc27c656c0790730af
gpf192 <1921183217@qq.com>
2020-11-22 22:00:41 +0800

    fix timeout

--- assets/js/mediumish.js
+++ assets/js/mediumish.js
@@ -118,12 +118,12 @@ var loadDeferredStyles = function () {
 	var addStylesNode = document.getElementById("deferred-styles");
 	var replacement = document.createElement("div");
 	replacement.innerHTML = addStylesNode.textContent;
-	document.body.appendChild(replacement);
+	//document.body.appendChild(replacement);
 	addStylesNode.parentElement.removeChild(addStylesNode);
 };
 var raf = window.requestAnimationFrame || window.mozRequestAnimationFrame ||
 	window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
 if (raf) raf(function () {
-	window.setTimeout(loadDeferredStyles, 0);
+	//window.setTimeout(loadDeferredStyles, 0);
 });
 else window.addEventListener('load', loadDeferredStyles);
